a70192da882a72c76840f1ff608ecffafc4a106b
Camilo Pinzon <pinzonac@gmail.com>
2024-01-22 16:18:35 -0500

    Validate invoice map

--- src/invoice_generator.clj
+++ src/invoice_generator.clj
@@ -1,6 +1,8 @@
 (ns invoice_generator
   (:require [clojure.data.json :as json]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [clojure.spec.alpha :as alpha]
+            [invoice-spec :as invoice-spec]))
 
 (defn read-json-from-file
   [file-name]
@@ -15,6 +17,12 @@
   (json/read-str (str json-data) :key-fn keyword)
 )
 
+(defn validate-invoice
+  [invoice]
+  (alpha/valid? ::invoice-spec/invoice invoice )
+)
+
 (def json-data (read-json-from-file "../invoice.json"))
 (def invoice-map (json-to-map json-data))
+(def invoice-map (validate-invoice invoice-map))
 (println "JSON Data:" invoice-map)
